0e7e62142c0dd6b701e1bf4f702d8836c4d31213
Xereer <gleb.romanov2002@mail.ru>
2023-07-31 00:26:56 +0400

    changed read

--- CRUD/read.php
+++ CRUD/read.php
@@ -1,8 +1,17 @@
 <?php
 
-$sql = "SELECT * FROM `university` WHERE `isArchive` = '0'";
-$stmt = $pdo->query($sql);
-
-while ($name = $stmt->fetch(PDO::FETCH_ASSOC)){
-    echo ($name['id'])." ".($name['name']). '<br>';
+function read($id){
+    global $pdo;
+    $sql = "SELECT id, name FROM university WHERE parentID = $id AND isArchive = 0";
+    $stmt = $pdo->query($sql);
+    $child = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($child);
+    // die;
+    foreach ($child as $value) {
+        echo ($value['id'])." ".($value['name']). '<br>';
+        read($value['id']);
+    }
+    
 }
+
+read (0);
