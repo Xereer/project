11cc0ec1fd39cbe9fb6805dd9041239b23283852
Xereer <gleb.romanov2002@mail.ru>
2023-08-09 20:34:35 +0300

    deleted connect and check

--- emptyCheck.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function checkVariables(...$variables) {
-    foreach ($variables as $variable) {
-        if (empty($variable)) {
-            throw new Exception("Ошибка: Поле не должно быть пустым.");
-        }
-    }
-}
